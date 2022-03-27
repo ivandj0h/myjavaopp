0f4247a215c01294b19287abef338a3e9224f8f1
ivandjoh <ivandi.djoh@gmail.com>
2022-03-27 22:17:03 +0800

    make class

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,13 @@
+package com.tutorial;
+
+// Membuat Class sebagai template untuk menampilkan output
+class Mahasiswa {
+
+}
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
