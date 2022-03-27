7e0d842a605221fb236f88d0939f4350fd2330af
ivandjoh <ivandi.djoh@gmail.com>
2022-03-27 22:58:02 +0800

    added constructor

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,13 +1,34 @@
 package com.tutorial;
 
 // Membuat Class sebagai template untuk menampilkan output
-class Mahasiswa {
+class Profile {
+    String nama;
+    String alamat;
+    int umur;
+
+    // Constructor tanpa parameter
+    Profile() {
+        System.out.println("ini adalah constructor tanpa parameter");
+    }
+
+    // Constructor dengan parameter
+    Profile(String nama, String alamat, int umur) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+
+        System.out.println("Nama : " + this.nama);
+        System.out.println("Alamat : " + this.alamat);
+        System.out.println("Umur : " + this.umur);
+    }
 
 }
 
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Membuat objek dari class Profile
+        new Profile("Budi", "Jakarta", 20);
+        
     }
 }
